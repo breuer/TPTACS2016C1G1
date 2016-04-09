ed3f33666d531a45246bf96c89ea426c2366a62d
breuer <llrbreuer@gmail.com>
2016-04-09 03:29:47 -0300

    agrego la aplicacion base

---
+++ tacs2016c1/src/main/java/com/g1/config/AppConfig.java
@@ -0,0 +1,11 @@
+package com.g1.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan({"com.g1.web"})
+public class AppConfig {
+}
